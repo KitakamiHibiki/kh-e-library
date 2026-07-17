a8bc1a5ab3ff52f74162beed6856f598ddda3900
kitakami_hibiki <kitakami_hibiki@users.noreply.github.com>
2026-07-17 13:38:45 +0800

    chore: bump to v0.0.2

--- build.bat
+++ build.bat
@@ -9,7 +9,8 @@ call npm.cmd install
 call npm.cmd run build
 
 echo === Copying frontend dist to backend ===
-if not exist "%~dp0backend\web\dist" mkdir "%~dp0backend\web\dist"
+if exist "%~dp0backend\web\dist" rmdir /S /Q "%~dp0backend\web\dist"
+mkdir "%~dp0backend\web\dist"
 xcopy /E /Y "%~dp0client\web\dist" "%~dp0backend\web\dist\"
 
 echo === Building backend ===
@@ -18,3 +19,4 @@ go build -ldflags="-s -w -X main.Version=%VERSION%" -o kh-e-library.exe ./cmd/se
 
 echo === Done! Binary: backend\e-library.exe
 pause
+
